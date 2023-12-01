1a56385f4840b201de995d1d4f11d631f30e701b
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-12-01 16:55:07 +0700

    chore(skaja-client): make binary use 2 clients

--- skaja-client/src/main.rs
+++ skaja-client/src/main.rs
@@ -1,22 +1,16 @@
 pub use skaja_client::Client;
 use skaja_lib::Command;
-use std::env;
 
 fn main() -> Result<(), String> {
     let mut client = Client::connect("127.0.0.1:8000".parse().unwrap());
-    let command = Command::try_from(env::args())?;
-
-    match client.send(command) {
-        Ok(response) => println!("{}", response),
-        Err(msg) => eprintln!("Failed sending command: {}", msg),
-    }
+    let mut client2 = Client::connect("127.0.0.1:8000".parse().unwrap());
 
     match client.send(Command::Set("hello".to_string(), "world".to_string())) {
         Ok(response) => println!("{}", response),
         Err(msg) => eprintln!("Failed sending command: {}", msg),
     }
 
-    match client.send(Command::Get("hello".to_string())) {
+    match client2.send(Command::Get("hello".to_string())) {
         Ok(response) => println!("{}", response),
         Err(msg) => eprintln!("Failed sending command: {}", msg),
     }
