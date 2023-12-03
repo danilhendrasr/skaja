6f4f1d3d1035fcf73a3ca5d7d1c7b77bac28ff19
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-12-03 19:53:42 +0700

    feat(skaja-client): turn it into REPL

--- skaja-client/src/main.rs
+++ skaja-client/src/main.rs
@@ -1,25 +1,42 @@
+use std::io::{self, Write};
+
+use clap::Parser;
 pub use skaja_client::Client;
 use skaja_lib::Command;
 
+#[derive(clap::Parser)]
+pub struct Args {
+    /// The address to bind to, defaults to 127.0.0.1:3000.
+    #[arg(long)]
+    host: String,
+}
+
 fn main() -> Result<(), String> {
-    let mut client = Client::connect("127.0.0.1:8000".parse().unwrap());
-    let mut client2 = Client::connect("127.0.0.1:8000".parse().unwrap());
-    let mut client3 = Client::connect("127.0.0.1:8000".parse().unwrap());
+    let args = Args::parse();
+    let mut client = Client::connect(args.host.parse().unwrap());
 
-    match client.send(Command::Set("hello".to_string(), "world".to_string())) {
-        Ok(response) => println!("Client 1 sent set command: {}", response),
-        Err(msg) => eprintln!("Failed sending command: {}", msg),
+    loop {
+        print!("skaja > ");
+        // Stdout is buffered, therefore the print above won't be printed until
+        // the code below is done reading from stdin. Flushing stdout will
+        // force it to be printed immediately.
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim().to_string();
+
+        let command = match Command::try_from(input.clone()) {
+            Ok(command) => command,
+            Err(_) => {
+                println!("Unrecognized command: {}.", input);
+                continue;
+            }
+        };
+
+        match client.send(command) {
+            Ok(response) => println!("{}", response),
+            Err(error) => println!("Error: {}.", error),
+        }
     }
-
-    match client2.send(Command::Get("hello".to_string())) {
-        Ok(response) => println!("Client 2 received: {}", response),
-        Err(msg) => eprintln!("Failed sending command: {}", msg),
-    }
-
-    match client3.send(Command::Get("hello".to_string())) {
-        Ok(response) => println!("Client 3 received: {}", response),
-        Err(msg) => eprintln!("Failed sending command: {}", msg),
-    }
-
-    Ok(())
 }
