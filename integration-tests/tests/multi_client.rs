1af344f726d466b70dfb2f063c680ab468926dc6
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-12-03 20:05:51 +0700

    feat(integration-tests): more multi-client test

--- integration-tests/tests/multi_client.rs
+++ integration-tests/tests/multi_client.rs
@@ -22,3 +22,37 @@ pub fn client1_set_command_result_should_be_visible_to_client2() {
         assert_eq!(response.message(), "world".to_string());
     })
 }
+
+#[test]
+pub fn client1_del_command_result_should_be_visible_to_client2() {
+    with_server(|server_address| {
+        let mut client1 = new_client(&server_address);
+        let mut client2 = new_client(&server_address);
+
+        client1
+            .send(skaja_lib::Command::Set(
+                "hello".to_string(),
+                "world".to_string(),
+            ))
+            .unwrap();
+
+        let response = client2
+            .send(skaja_lib::Command::Get("hello".to_string()))
+            .unwrap();
+
+        let status_code = response.status_code();
+        assert_eq!(status_code, skaja_lib::StatusCodes::Ok);
+        assert_eq!(response.message(), "world".to_string());
+
+        client1
+            .send(skaja_lib::Command::Delete("hello".to_string()))
+            .unwrap();
+
+        let response = client2
+            .send(skaja_lib::Command::Get("hello".to_string()))
+            .unwrap();
+
+        let status_code = response.status_code();
+        assert_eq!(status_code, skaja_lib::StatusCodes::ClientErr);
+    })
+}
